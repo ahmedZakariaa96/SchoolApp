104366cdff6ed90e8b6db3df9c716a01aaaf2706
ahmedzakariaa96 <ahmedzakariaa96@gmail.com>
2024-11-22 20:17:19 +0200

    Add DB Seed

---
+++ School.Infrestructure/Persistence/Seed/DBSeed.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities;
+using School.Domain.Entities.IdentityServer;
+using System.Text.Json;
+
+namespace School.Infrestructure.Persistence.Seed
+{
+    public static class DBSeed
+    {
+        public static async Task SeedData(ApplicationDbContext context)
+        {
+
+
+            await ReadData<Department>("Department", context);
+            await ReadData<Student>("Student", context);
+            //------------------------------------------------
+
+
+
+
+        }
+        public static async Task ReadData<TEntity>(string fileName, ApplicationDbContext context) where TEntity : class
+        {
+            if (!await context.Set<TEntity>().AnyAsync())
+            {
+                // Get the base path of the project directory
+                var basePath = Path.Combine(Directory.GetCurrentDirectory(), @"..\");
+
+                // Combine it with your desired path
+                var filePath = Path.Combine(basePath, "School.Infrestructure", "Persistence", "Seed", "SeedData", $"{fileName}.json");
+
+                // Read the file asynchronously
+                var file = await File.ReadAllTextAsync(filePath);
+
+                var data = JsonSerializer.Deserialize<List<TEntity>>(file);
+
+                await context.Set<TEntity>().AddRangeAsync(data);
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public static async Task<List<TEntity>?> ReadDataOnly<TEntity>(string fileName) where TEntity : class
+        {
+
+            // Get the base path of the project directory
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), @"..\");
+
+            // Combine it with your desired path
+            var filePath = Path.Combine(basePath, "School.Infrestructure", "Persistence", "Seed", "SeedData", $"{fileName}.json");
+
+            // Read the file asynchronously
+            var file = await File.ReadAllTextAsync(filePath);
+
+            var data = JsonSerializer.Deserialize<List<TEntity>>(file);
+
+            return data;
+        }
+
+
+        //------------------------------------------------
+        public static async Task SeedUsers(UserManager<User> _userManager, RoleManager<Role> _roleManager)
+
+        {
+            var firstAdded = !_userManager.Users.Any() && !_roleManager.Roles.Any();
+
+            if (!_userManager.Users.Any())
+            {
+                var Users = await ReadDataOnly<User>("User");
+                if (Users != null && Users.Any())
+                {
+
+
+                    foreach (var user in Users)
+                    {
+
+                        await _userManager.CreateAsync(user, user.PasswordHash);
+                    }
+                }
+
+            }
+
+            if (!_roleManager.Roles.Any())
+            {
+                var roles = await ReadDataOnly<Role>("Role");
+                if (roles != null && roles.Any())
+                {
+
+                    foreach (var role in roles)
+                    {
+
+                        await _roleManager.CreateAsync(role);
+                    }
+                }
+
+            }
+
+            if (firstAdded)
+            {
+                var adminUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == "admin");
+                var adminRole = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Name.ToLower() == "admin");
+                if (adminUser != null && adminRole != null)
+                {
+                    var userRoleResult = await _userManager.AddToRoleAsync(adminUser, adminRole.Name);
+
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
--- School/Program.cs
+++ School/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using School.Application;
 using School.Application.Base.Middleware;
@@ -8,6 +9,7 @@ using School.Domain.Entities;
 using School.Domain.Entities.IdentityServer;
 using School.Infrestructure;
 using School.Infrestructure.Persistence;
+using School.Infrestructure.Persistence.Seed;
 using System.Globalization;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -110,6 +112,22 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    #region Automatic Migrations Applying & Seeding Default Users And Roles -- Seeding Cancelled
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
+        await DBSeed.SeedData(db);
+
+
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+        await DBSeed.SeedUsers(userManager, roleManager);
+
+
+    }
+    #endregion
 }
 
 app.UseHttpsRedirection();
