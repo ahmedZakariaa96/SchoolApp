10ca6e4b777a814ea8f8405efaf984f4035d8e95
ahmedzakariaa96 <ahmedzakariaa96@gmail.com>
2025-01-03 22:24:34 +0200

    push migration file

---
+++ School.Infrestructure/Persistence/PowerShell Script/create-migration.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+set migrationName=%1
+if "%migrationName%"=="" (
+    echo Please provide a migration name.
+    exit /b 1
+)
+dotnet ef migrations add %migrationName% -OutputDir Persistence/Migrations -Context ApplicationDbContext
---
+++ School.Infrestructure/Persistence/PowerShell Script/create-migration.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [string]$MigrationName
+)
+
+if (-not $MigrationName) {
+    Write-Host "Please provide a migration name."
+    exit 1
+}
+
+dotnet ef migrations add $MigrationName -OutputDir Persistence/Migrations -Context ApplicationDbContext
